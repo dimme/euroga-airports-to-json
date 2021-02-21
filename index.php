8d783fb01d471582199db104c29c473f101becec
Dimitrios Vlastaras <dimitrios@cryodev.se>
2021-02-21 05:40:15 +0100

    Added index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EuroGA Airport to JSON API</title>
+</head>
+<body>
+    <a href="search.php?icao=ESME&country=&type=report&date_from_pick=&date_from=&date_to_pick=&date_to=&runway_condition=&max_landing_fee=&all=">Search example</a>
+    <a href="record.php?id=364">Record example</a>
+</body>
+</html>
