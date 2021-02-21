bce51c6eff30c3247fa3907bc20fc06ead05e0e1
Dimitrios Vlastaras <dimitrios@cryodev.se>
2021-02-21 05:48:51 +0100

    Better examples

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>EuroGA Airport to JSON API</title>
 </head>
 <body>
-    <p><a href="search.php?icao=ESME&country=&type=report&date_from_pick=&date_from=&date_to_pick=&date_to=&runway_condition=&max_landing_fee=&all=">Search example</a></p>
-    <p><a href="record.php?id=364">Record example</a></p>
+    <p><a href="search.php?icao=ESMS&country=&type=report&date_from_pick=&date_from=&date_to_pick=&date_to=&runway_condition=&max_landing_fee=&all=">Search example</a></p>
+    <p><a href="record.php?id=476">Record example</a></p>
 </body>
 </html>
